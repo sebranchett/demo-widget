d4150a5acaaf7a15c7484c13c2fc93188ed8f4d5
sebranchett <s.e.branchett@tudelft.nl>
2025-02-28 11:43:21 +0100

    tidy up for screencast demo

--- src/script.js
+++ src/script.js
@@ -3,8 +3,8 @@ const DOT_RADIUS = 10; // Radius of terminal dots
 export function render({ model, el }) {
     let canvas = document.createElement("canvas");
     canvas.id = 'circuitCanvas';
-    canvas.width = 400;
-    canvas.height = 300;
+    canvas.width = 200;
+    canvas.height = 200;
     canvas.style.border = '1px solid black';
     let ctx = canvas.getContext("2d");
 
